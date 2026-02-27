e4cc571c9fcec823f1f6f63d42cf7a03540d8057
Yasir-khan <techpro54321@gmail.com>
2026-02-27 09:38:00 +0500

    Update server.js
    l

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:'https://yasir-khan-legent.github.io/Advance-Note-App-Frontend/',
+    origin:'https://yasir-khan-legent.github.io/Advance-Note-App-Frontend',
     credentials:true,
 }))
 Conection()
