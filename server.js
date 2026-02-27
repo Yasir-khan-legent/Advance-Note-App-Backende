c2c777af6a5569ea066ef5cc1a4adf4396fca30a
Yasir-khan <techpro54321@gmail.com>
2026-02-27 08:02:07 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import auth from './src/Routes/Auth.routes.js'
+import Notes from './src/Routes/Notes.routes.js'
+import Conection from './src/Config/Mongodb.config.js'
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
+Conection()
+
+
+app.use('/auth' ,auth )
+app.use('/notes' ,Notes )
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log('Server Is Running 👍')
+})
